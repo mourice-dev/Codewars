97abcb7fe2052d4efbbb2da5fba21bbfb87d5df3
Nshuti Maurice <nshutikope@gmail.com>
2025-11-25 18:25:25 +0200

    changes

---
+++ deplicateEncode.js
@@ -0,0 +1,9 @@
+/** @format */
+
+function duplicateEncode(word) {
+  word = word.toLowerCase();
+  return word
+    .split("")
+    .map((char) => (word.indexOf(char) === word.lastIndexOf(char) ? "(" : ")"))
+    .join("");
+}
