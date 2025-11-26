201a92634a58f0555c535224bb8c0f4d807a9183
Nshuti Maurice <nshutikope@gmail.com>
2025-11-26 18:33:02 +0200

    function callback

--- countingdepulicate.js
+++ countingdepulicate.js
@@ -4,3 +4,10 @@ function duplicateCount(text) {
   const t = text.toLowerCase();
   return [...new Set(t)].filter((c) => t.split(c).length - 1 > 1).length;
 }
+console.log(duplicateCount("abcde"));              // 0
+console.log(duplicateCount("aabbcde"));            // 2
+console.log(duplicateCount("aabBcde"));            // 2
+console.log(duplicateCount("indivisibility"));     // 1
+console.log(duplicateCount("Indivisibilities"));   // 2
+console.log(duplicateCount("aA11"));               // 2
+console.log(duplicateCount("ABBA"));               // 2
