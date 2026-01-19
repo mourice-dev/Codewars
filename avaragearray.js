6393a5c2d64f15424a978458f0057b637e6308c7
Nshuti Maurice <nshutikope@gmail.com>
2026-01-19 12:00:00 +0200

    Add solution for avaragearray kata

---
+++ avaragearray.js
@@ -0,0 +1,17 @@
+// ASC Week 1 Challenge 5 (Medium #2)
+// Create a function that takes a 2D array as an input, and outputs another array that contains the average values for the numbers in the nested arrays at the corresponding indexes.
+
+// Note: the function should also work with negative numbers and floats.
+
+// Examples
+// [ [1, 2, 3, 4], [5, 6, 7, 8] ]  ==>  [3, 4, 5, 6]
+
+// 1st array: [1, 2, 3, 4]
+// 2nd array: [5, 6, 7, 8]
+//             |  |  |  |
+//             v  v  v  v
+// average:   [3, 4, 5, 6]
+
+function avgArray(arr) {
+
+}
